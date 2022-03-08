136aa3db1b2bb705a3c982213b7b235ad0ac932b
Hasanzoda Muhammadiqbol <foretde1999@gmail.com>
2022-03-08 12:00:07 +0500

    tic rac toe

--- Algorithm/ticTacToeChecker.js
+++ Algorithm/ticTacToeChecker.js
@@ -15,27 +15,121 @@ We want our function to return:
 You may assume that the board passed in is valid in the context of a game of Tic-Tac-Toe.
  */
 
-function isSolved(array) {
-  let whoWin = -1;
-
-  for (let i = 0; i < array.length; i++) {
-    const win = array[i].reduce(function (acc, curr) {
-      return acc + curr;
-    }, 0);
-    console.log(win);
-    // if () {
-    // }
-  }
-
-  for (let i = 0; i < array.length; i++) {
-    for (let j = 0; j < array[i].length; j++) {
-      console.log(array[j][i]);
+function down(arr) {
+  let reversed = [];
+  for (let i = 0; i < arr.length; i++) {
+    let m = [];
+    for (let j = 0; j < arr.length; j++) {
+      m.push(arr[j][i]);
     }
+    reversed.push(m);
   }
+  return reversed;
 }
 
-isSolved([
-  [0, 0, 1],
-  [0, 1, 2],
-  [2, 1, 0],
-]);
+function left(arr) {
+  let result = [];
+  for (let i = 0; i < arr.length; i++) {
+    let m = [];
+    for (let j = 0; j < arr.length; j++) {
+      m.push(arr[i][j]);
+    }
+    result.push(m.reverse());
+  }
+  return result;
+}
+
+function isSolved(params = [[]]) {
+  let diag = [];
+  let win = 0;
+  for (let i = 0; i < params.length; i++) {
+    if (params[i].every((e) => e === 1)) {
+      return 1;
+    }
+    if (params[i].every((e) => e === 2)) {
+      return 2;
+    }
+    if (params[i].some((e) => e === 0)) {
+      win = -1;
+    }
+    for (let j = 0; j < params.length; j++) {
+      if (i === j) {
+        diag.push(params[i][j]);
+      }
+    }
+  }
+
+  console.log(diag);
+  if (diag.every((e) => e === 1)) {
+    return 1;
+  }
+  if (diag.every((e) => e === 2)) {
+    return 2;
+  }
+  if (diag.some((e) => e === 0)) {
+    win = -1;
+  }
+  if (win !== 0) {
+    return win;
+  }
+  diag = [];
+  const l = left(params);
+  for (let i = 0; i < l.length; i++) {
+    if (l[i].every((e) => e === 1)) {
+      win = 1;
+    }
+    if (l[i].every((e) => e === 2)) {
+      win = 2;
+    }
+    if (l[i].some((e) => e === 0)) {
+      win = -1;
+    }
+    for (let j = 0; j < l.length; j++) {
+      if (i === j) {
+        if (i === j) {
+          diag.push(l[i][j]);
+        }
+      }
+    }
+  }
+  if (win !== 0) {
+    return win;
+  }
+  if (diag.every((e) => e === 1)) {
+    return 1;
+  }
+  if (diag.every((e) => e === 2)) {
+    return 2;
+  }
+  if (diag.some((e) => e === 0)) {
+    win = -1;
+  }
+  if (win !== 0) {
+    return win;
+  }
+  let d = down(params);
+  for (let i = 0; i < d.length; i++) {
+    if (d[i].every((e) => e === 1)) {
+      return 1;
+    }
+    if (d[i].every((e) => e === 2)) {
+      return 2;
+    }
+    if (d[i].some((e) => e === 0)) {
+      win = -1;
+    }
+  }
+  if (win !== 0) {
+    return win;
+  }
+
+  return 0;
+}
+
+console.log(
+  isSolved([
+    [1, 2, 0],
+    [0, 1, 2],
+    [0, 0, 1],
+  ])
+);
