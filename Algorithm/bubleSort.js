cccd2596a67b7e6b3a5c88a2cc102e72862b4d2f
Hasanzoda Muhammadiqbol <foretde1999@gmail.com>
2022-07-09 21:23:40 +0500

    инитиал комит

--- Algorithm/bubleSort.js
+++ Algorithm/bubleSort.js
@@ -1,13 +1,19 @@
-const bubbleSortArr = [2, 3, 0, 4, 1, 13, 43];
+/**
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ */
+var merge = function (nums1, m, nums2, n) {
+	nums2.forEach((el) => {
+		nums1.push(el);
+	});
+	nums1.some((a, b) => b - a);
+	nums1 = nums1.filter((f) => f);
+	console.log(nums1);
+};
 
-for (let i = 0; i < bubbleSortArr.length; ++i) {
-  for (let j = 0; j < bubbleSortArr.length - j; ++j) {
-    if (bubbleSortArr[j] > bubbleSortArr[j + 1]) {
-      let temp = bubbleSortArr[j];
-      bubbleSortArr[j] = bubbleSortArr[j + 1];
-      bubbleSortArr[j + 1] = temp;
-    }
-  }
-}
+// nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
 
-console.log(bubbleSortArr);
+console.log(merge([1, 2, 3, 0, 0, 0], 3, [2, 5, 6], 3));
--- Algorithm/main.go
+++ Algorithm/main.go
@@ -1,23 +1,9 @@
 package main
 
-import "fmt"
-
 func main() {
-	var a int
-	var b int
-	var c *int
-	a = 3
-	b = a
-	c = &a
 
-	a = 1
-
-	fmt.Println("a", a)
-	fmt.Println("b", b)
-	fmt.Println("c", *c)
 }
 
-func Calc(a, b int) (result int) {
-	result = a + b
-	return
+func twoSum()  {
+	
 }
